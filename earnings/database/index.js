42beff0b1a3cbbe79eec242706f42b1dd21d1afe
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-26 20:37:37 -0700

    Implement CRUD routes from client to server to Postgres database
    - curl -X DELETE localhost:8080/stocks/:70000001
    - curl -X UPDATE localhost:8080/stocks/:70000001
    - curl -X GET localhost:8080/api/earnings
    - Postman for POST route
    
    Check if successful with psql
    - SELECT * FROM earnings WHERE earnings_id=70000000;
    - SELECT * FROM earnings WHERE earnings_id=70000001;

--- earnings/database/index.js
+++ earnings/database/index.js
@@ -5,20 +5,10 @@ const pool = new Pool({
     port: 5432,
     database: 'sdc_database',
     user: 'student',
-    password: 'student',
+    password: process.env.PSQL_PASSWORD || 'student',
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
   })
 
-pool.query(
-    `SELECT * FROM earnings 
-    WHERE earnings_id=10`
-    , (err, res) => {
-        if (err) {
-            console.log(err.error);
-        } else {
-            console.log(res.rows)
-        }    
-    pool.end()
-});
+module.exports = pool;
